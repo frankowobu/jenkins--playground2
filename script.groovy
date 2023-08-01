412c697982f0556a552fb6607536e4daba8592f6
aize <frankowobu11@gmail.com>
2023-08-01 16:00:28 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,11 +2,6 @@ def buildApp(){
   echo "Building the application for version ${params.VERSION}..."
 }
 def testApp(){
-  when{
-    expression{
-      params.executeTests
-    }
-  }
    echo "Testing the application..."
 }
 def deployApp(){
